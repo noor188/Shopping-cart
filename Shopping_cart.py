870b87dce4e2b6c9e2b9fa8fe3efaa5d155fd03c
Noor <noormmlk@yahoo.com>
2023-06-06 06:00:26 -0600

    Adding a while loop
    making cart private , creating an infinite while loop that keeps prompting the user for an input

--- Shopping_cart.py
+++ Shopping_cart.py
@@ -1,28 +1,49 @@
-# Shopping cart application 
+# Shopping cart application
 # store / items / carts
 
 class Item:
-  def __init__(self, name, price):
-    self.name = name   
-    self.price = price 
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
 
 class Store:
-  def __init__(self):
-    self.cart = []
+    def __init__(self):
+        self.__cart = []
 
-  def addToCart(item):
-    cart.append(item)
+    def addToCart(self, item):
+        self.__cart.append(item)
 
-  def removeFromCart(index):
-    cart.pop(index)
+    def removeFromCart(self, index):
+        self.__cart.pop(index)
 
-  def calculateTotal(cart):
-    total = 0
-    for item in cart:
-      total += item.price
-    
-    return total
+    def calculateTotal(self):
+        total = 0
+        for item in self.__cart:
+            total += item.price
+
+        return total
+
+    def displayCart(self):
+        for indx in range(0, len(self.__cart)):
+            print(indx, self.__cart[indx].name)
+
 
 store = Store()
-store.addToCart(Item('cup', 10))
-print(store.cart)
+print("Welcome to our store")
+
+while True:
+    store.displayCart()
+    store.calculateTotal()
+    print("Press a to add item")
+    print("press r to remove item")
+    print("Press x to exit")
+
+    userInput = input("Please enter a command")
+
+    if userInput == "x":
+        break
+    elif userInput == "a":
+        itemName = input("Please give the item a name")
+        itemPrice = input("Please give the item a price")
+        store.addToCart()
